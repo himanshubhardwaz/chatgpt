14070fe7d4206e57fd70bbc941e60ce2621f219e
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-12-27 18:11:01 +0530

    new app directory

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// These styles apply to every route in the application
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
