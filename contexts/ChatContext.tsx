4b2125205ddf2688ec5f282d083c017dfab26180
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-12-30 19:15:36 +0530

    use reducer logic

--- contexts/ChatContext.tsx
+++ contexts/ChatContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, createContext, ReactNode } from "react";
+import { useContext, createContext, ReactNode, useReducer } from "react";
 
 interface SingleChatInterface {
   id: string;
@@ -12,19 +12,87 @@ interface ChatInterface {
   currentActiveChat: string;
 }
 
-const initialValue = {
-  chats: [],
-  currentActiveChat: "",
-};
+interface ChatStateAndMethods extends ChatInterface {
+  getCurrentActiveChat: () => SingleChatInterface;
+}
 
-const ChatCtx = createContext<ChatInterface>(initialValue);
+enum ChatActionType {
+  UPDATE_CURRENT_CHAT = "UPDATE_CURRENT_CHAT",
+  DELETE_CURRENT_CHAT = "DELETE_CURRENT_CHAT",
+  CREATE_NEW_CHAT = "CREATE_NEW_CHAT",
+  DELETE_ALL_CHAT = "DELETE_ALL_CHAT",
+}
+
+type ChatAction =
+  | { type: ChatActionType.CREATE_NEW_CHAT }
+  | { type: ChatActionType.UPDATE_CURRENT_CHAT; payload: SingleChatInterface }
+  | { type: ChatActionType.DELETE_CURRENT_CHAT; payload: SingleChatInterface }
+  | { type: ChatActionType.DELETE_ALL_CHAT };
+
+const ChatCtx = createContext<ChatStateAndMethods>(null!);
 
 export const useChatCtx = () => {
-  return useContext<ChatInterface>(ChatCtx);
+  return useContext<ChatStateAndMethods>(ChatCtx);
 };
 
 const ChatProvider = ({ children }: { children: ReactNode }) => {
-  return <ChatCtx.Provider value={initialValue}>{children}</ChatCtx.Provider>;
+  const getCurrentActiveChat = () => {
+    return { id: "", chat: [] };
+  };
+
+  const initialValue = {
+    chats: [],
+    currentActiveChat: "",
+  };
+
+  const reducer = (state: ChatInterface, action: ChatAction): ChatInterface => {
+    switch (action.type) {
+      case ChatActionType.CREATE_NEW_CHAT:
+        return {
+          ...state,
+          chats: [...state.chats, { id: self.crypto.randomUUID(), chat: [] }],
+        };
+      case ChatActionType.UPDATE_CURRENT_CHAT: {
+        return {
+          ...state,
+          chats: state.chats.map((chat) => {
+            if (chat.id === action.payload.id) {
+              return action.payload;
+            }
+            return chat;
+          }),
+        };
+      }
+      case ChatActionType.DELETE_CURRENT_CHAT: {
+        const updatedChats: SingleChatInterface[] = [];
+
+        state.chats.forEach((chat) => {
+          if (chat.id !== action.payload.id) {
+            updatedChats.push(chat);
+          }
+        });
+
+        return {
+          ...state,
+          chats: updatedChats,
+        };
+      }
+      case ChatActionType.DELETE_ALL_CHAT: {
+        return initialValue;
+      }
+      default: {
+        return state;
+      }
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialValue);
+
+  return (
+    <ChatCtx.Provider value={{ ...state, getCurrentActiveChat }}>
+      {children}
+    </ChatCtx.Provider>
+  );
 };
 
 export default ChatProvider;
